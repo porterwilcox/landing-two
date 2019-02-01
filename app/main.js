91c553c9b5a6fada6503c996fcbfd59699b5f515
porterwilcox <wilcox.porter@gmail.com>
2019-01-31 20:49:38 -0700

    side nav collapse on click and transformicon revert

--- app/main.js
+++ app/main.js
@@ -1,13 +1,14 @@
 //thanks tcons!
 !function(r,n){"function"==typeof define&&define.amd?define(n):"object"==typeof exports?module.exports=n():r.transformicons=n()}(this||window,function(){"use strict";var r={},n={transform:["click"],revert:["click"]},t=function(r){return"string"==typeof r?Array.prototype.slice.call(document.querySelectorAll(r)):void 0===r||r instanceof Array?r:[r]},o=function(r){return"string"==typeof r?r.toLowerCase().split(" "):r},e=function(r,e,f){var i=(f?"remove":"add")+"EventListener",s=t(r),a=s.length,u={};for(var l in n)u[l]=e&&e[l]?o(e[l]):n[l];for(;a--;)for(var d in u)for(var m=u[d].length;m--;)s[a][i](u[d][m],c)},c=function(n){r.toggle(n.currentTarget)};return r.add=function(n,t){return e(n,t),r},r.remove=function(n,t){return e(n,t,!0),r},r.transform=function(n){return t(n).forEach(function(r){r.classList.add("tcon-transform")}),r},r.revert=function(n){return t(n).forEach(function(r){r.classList.remove("tcon-transform")}),r},r.toggle=function(n){return t(n).forEach(function(n){r[n.classList.contains("tcon-transform")?"revert":"transform"](n)}),r},r});
-transformicons.add(".tcon")
+let formicon = transformicons.add(".tcon")
 
+//side-nav-btn color changing script
 let offsetTopPos = document.querySelector('.slant-about').offsetTop
 
 window.addEventListener('scroll', checkPosition)
 
-function checkPosition(e) {
-    let curPos = e.target.scrollingElement.scrollTop
+function checkPosition() {
+    let curPos = window.scrollY
     if (curPos < offsetTopPos - 40) {
         document.querySelector('.side-nav-btn').style.background = 'transparent'
     } else {
@@ -15,3 +16,12 @@ function checkPosition(e) {
         document.querySelector('.side-nav-btn').style.borderRadius = '15%'
     }
 }
+checkPosition()
+
+//collapse side-nav on click
+let sideNav = document.querySelector('.side-nav')
+
+sideNav.addEventListener('click', (e) => {
+    $('.collapse').collapse('hide')
+    formicon.revert('.tcon-menu--xbutterfly')
+})
